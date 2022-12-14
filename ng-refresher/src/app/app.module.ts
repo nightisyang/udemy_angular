6739889a27deb21fa6117ec4cbb47c475a456564
nightisyang <shenyangc86@gmail.com>
2022-12-14 17:17:44 +0800

    learning basic angular concepts

---
+++ ng-refresher/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './person-input/person-input.component';
+
+@NgModule({
+  declarations: [AppComponent, PersonsComponent, PersonInputComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
